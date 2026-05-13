d96a41af10661f0ec33ceb2589b44f62b4d570ba
ac144228 <pranav.kumar@imw.uni-stuttgart.de>
2026-05-13 14:28:13 +0200

    Move ase2force test data into repo

--- tests/common/test_ase2force.py
+++ tests/common/test_ase2force.py
@@ -1,38 +1,29 @@
-import os
 import subprocess
-import urllib.error
-import urllib.request
+from pathlib import Path
 
 import pytest
 
 
-OUTCAR_URL = 'https://raw.githubusercontent.com/keeeto/VASP-Elastic/refs/heads/master/OUTCAR'
-OUTCAR_005_URL = 'https://raw.githubusercontent.com/keeeto/VASP-Elastic/master/OUTCAR.005'
+DATA_DIR = Path(__file__).resolve().parents[1] / 'data' / 'ase2force'
 
 
 def _run_converter(script, outcar):
     result = subprocess.run(
-        [script, '-f', outcar],
+        [str(script), '-f', str(outcar)],
         check=False,
         capture_output=True,
         text=True,
-        cwd=os.path.dirname(os.path.dirname(__file__)),
+        cwd=Path(__file__).resolve().parents[2],
     )
     return result
 
 
-@pytest.mark.parametrize('outcar_url', [OUTCAR_URL, OUTCAR_005_URL])
-def test_ase2force_matches_vasp2force(tmp_path, outcar_url):
-    outcar = tmp_path / 'OUTCAR'
-    try:
-        with urllib.request.urlopen(outcar_url, timeout=30) as response:
-            outcar.write_bytes(response.read())
-    except (urllib.error.URLError, TimeoutError, OSError) as exc:
-        pytest.skip(f'Could not download sample OUTCAR: {exc}')
-
-    root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    ase2force = os.path.join(root, 'util', 'ase2force')
-    vasp2force = os.path.join(root, 'util', 'vasp2force')
+@pytest.mark.parametrize('outcar_name', ['OUTCAR_example_1', 'OUTCAR_example_1.gz'])
+def test_ase2force_matches_vasp2force(outcar_name):
+    outcar = DATA_DIR / outcar_name
+    root = Path(__file__).resolve().parents[2]
+    ase2force = root / 'util' / 'ase2force'
+    vasp2force = root / 'util' / 'vasp2force'
 
     ase_result = _run_converter(ase2force, str(outcar))
     vasp_result = _run_converter(vasp2force, str(outcar))
